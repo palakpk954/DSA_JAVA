e882f2a95e97571dc719469ab92469df51560f2d
palakpk954 <157914156+palakpk954@users.noreply.github.com>
2026-03-13 01:08:06 +0530

    Add files via upload

---
+++ reverse_a_queue.java
@@ -0,0 +1,25 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class reverse_a_queue {
+    static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        System.out.println(q);
+        Stack<Integer> st = new Stack<>();
+        while(q.size()>0){
+            st.push(q.remove());
+        }
+        while(st.size()>0){
+            q.add(st.pop());
+        }
+        System.out.println(q);
+
+    }
+}
